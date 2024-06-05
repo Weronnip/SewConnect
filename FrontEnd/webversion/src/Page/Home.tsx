9927eb580fc588d6b3d4ff29b4bce4b90c70c46d
weronnip <wowo.vovo12345@yandex.ru>
2024-06-05 17:51:46 +0300

    add block for content site

--- FrontEnd/webversion/src/Page/Home.tsx
+++ FrontEnd/webversion/src/Page/Home.tsx
@@ -3,7 +3,15 @@ export function HomePage() {
     return (
         <>
         <section className={styles.section_about}>
-            <div className=""></div>
+            <div className="inline-block mx-[10px] absolute">
+                <div className={styles.block_content}></div>
+                <div className={styles.block_content}></div>
+                <div className={styles.block_content}></div>
+
+                <div className={styles.block_content}></div>
+                <div className={styles.block_content}></div>
+                <div className={styles.block_content}></div>
+            </div>
         </section>        
         </>
     );
