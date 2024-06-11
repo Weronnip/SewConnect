c02ba0ca06c919e7b7ecda7d7737c8c341e64aaf
weronnip <wowo.vovo12345@yandex.ru>
2024-06-11 14:49:24 +0300

    add import prodile module

--- server_back/src/app.service.ts
+++ server_back/src/app.service.ts
@@ -1,13 +1,8 @@
-import { HomeController } from './controllers/home.controller';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
-  }
-
-  getHome(): HomeController  {
-    return new HomeController();
   }
 }
