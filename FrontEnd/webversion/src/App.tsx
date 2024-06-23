1430383ec2fac7299f8d280c02e1149f70058c70
weronnip <wowo.vovo12345@yandex.ru>
2024-06-23 10:49:06 +0300

    add new route DownloadPage

--- FrontEnd/webversion/src/App.tsx
+++ FrontEnd/webversion/src/App.tsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom"
 import { Layout } from "./components/layout/layout"
 import { Error } from "./components/Error/error"
 import { HomePage } from "./Page/Home"
+import { DownloadPage } from "./Page/Download"
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <>
         <Routes>
           <Route path="/" element={<Layout/>}>
-            <Route index element={<HomePage/>} />
+            <Route index element={<HomePage/>}/>
+            <Route path="/download" element={< DownloadPage/>}/>
             <Route path="*" element={<Error/>}/>
           </Route>
         </Routes>
