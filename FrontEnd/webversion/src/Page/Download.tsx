f36e5898f5fe67b563a389398f079071ee73209d
weronnip <wowo.vovo12345@yandex.ru>
2024-06-23 11:06:40 +0300

    add component DownloadBlockComponentUI

--- FrontEnd/webversion/src/Page/Download.tsx
+++ FrontEnd/webversion/src/Page/Download.tsx
@@ -1,16 +1,13 @@
 import styles from "../Styles/style.module.css"
+import { DownloadBlockComponentUI } from "../components/downloads_componnets/comonents_download";
 
 export function DownloadPage() {
 
     return (
         <>
             <section className={styles.section_one}>
-                <div className="w-[90%] h-80 mx-[5%] my-11 bg-slate-700 rounded-xl
-                                absolute justify-center flex">
-                    <div className="bg-emerald-400 flex justify-center w-52 h-7 rounded-b-lg">
-                    <h3 className="text-balance">Download Sow Connection</h3>
-                    </div>
-                </div>
+                <DownloadBlockComponentUI/>
+                
             </section>  
         </>
     );
