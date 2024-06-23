5cbe287fdb78c3b8e34fbbd803c43b19f242783e
weronnip <wowo.vovo12345@yandex.ru>
2024-06-23 11:06:04 +0300

    add UI components block for Download page

---
+++ FrontEnd/webversion/src/components/downloads_componnets/comonents_download.tsx
@@ -0,0 +1,13 @@
+
+export function DownloadBlockComponentUI() {
+
+    return (
+        <>
+            <div className="w-[90%] h-80 mx-[5%] my-11 bg-slate-700 rounded-xl  absolute justify-center flex">
+                    <div className="bg-emerald-400 flex justify-center w-52 h-7 rounded-b-lg">
+                    <h3 className="text-balance">Download Sow Connection</h3>
+                    </div>
+            </div>
+        </>
+    );
+}
