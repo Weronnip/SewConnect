79db6a1e35f261b179b978a334470f1f717a7bb9
weronnip <wowo.vovo12345@yandex.ru>
2024-06-27 19:01:06 +0300

    none edits

--- FrontEnd/webversion/src/components/downloads_componnets/comonents_download.tsx
+++ FrontEnd/webversion/src/components/downloads_componnets/comonents_download.tsx
@@ -3,9 +3,9 @@ export function DownloadBlockComponentUI() {
 
     return (
         <>
-            <div className="w-[90%] h-80 mx-[5%] my-11 bg-slate-700 rounded-xl  absolute justify-center flex">
-                    <div className="bg-emerald-400 flex justify-center w-52 h-7 rounded-b-lg">
-                    <h3 className="text-balance">Download Sow Connection</h3>
+            <div className="w-[90%] h-80 mx-[5%] my-11 bg-slate-700 rounded-xl mt-5 pb-5 relative justify-center flex">
+                    <div className="bg-emerald-400 flex justify-center w-52 h-7 rounded-b-lg bg-fixed">
+                        <h3 className="text-balance">Download Sow Connection</h3>
                     </div>
             </div>
         </>
