4d045cda3998d11c6ce6e3a8a4ebe536b8bceda9
weronnip <wowo.vovo12345@yandex.ru>
2024-06-27 19:03:45 +0300

    add QueryClientProvider to components Products

--- FrontEnd/webversion/src/Page/Home.tsx
+++ FrontEnd/webversion/src/Page/Home.tsx
@@ -1,17 +1,20 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import styles from '../Styles/style.module.css'
 import { Products } from '../components/Products/Products.tsx';
 import { UpGrageNews } from '../components/news_company/update_product.tsx';
 
+const queryClient = new QueryClient();
 export function HomePage() {
     return (
         <>
         <section className={styles.section_one}>
             <div className={styles.cards}>
-                <div className="grid grid-rows-1 grid-cols-3 gap-9">
-                    <Products />
-                    <Products />
-                    <Products />
-                </div>
+                <QueryClientProvider client={queryClient}>
+                    <div className="grid grid-rows-1 grid-cols-3 gap-9">
+                        <Products />
+                    </div>
+                </QueryClientProvider>
                 <div className="">
                     <UpGrageNews />
                 </div>
